8910a6b9763f1637131a09c523fd38638902776e
rizwannadeem2017 <m_rizwan6@hotmail.com>
2022-02-27 17:16:30 +0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.instance_region
+}
+
+resource "aws_key_pair" "key_pair" {
+    key_name = "terraform_demo"
+    public_key = file("/Users/rizwannadeem/.ssh/id_rsa.pub") 
+}
+
+
+resource "aws_instance" "app" {
+  count = var.instance_count
+  ami         = var.ami
+  instance_type = var.instance_type
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update -y
+    sudo apt-get install nginx -y
+    sudo systemctl enable nginx
+    sudo systemctl start nginx
+    EOF
+  subnet_id  = data.aws_subnet.centtest-subnet-private.id
+  vpc_security_group_ids =  ["${data.aws_security_group.centtest-sg.id}"]
+  tags = {
+      name = "Terraform-${var.ami}-${var.ec2_name[count.index]}"
+  }
+  key_name = aws_key_pair.key_pair.key_name 
+}
+
+
+data "aws_subnet" "centtest-subnet-private" {
+  id = var.subnet_id_private.id
+}
+
+data "aws_security_group" "centtest-sg" {
+   id = var.security_group_id.id
+}
+
+resource "aws_eip" "app-eip" {
+    instance = "${element(aws_instance.app.*.id,count.index)}"
+    count = var.instance_count
+}
+output "DNS" {
+  value = aws_eip.app-eip.*.public_dns
+}
+
+output "IP" {
+  value = aws_eip.app-eip.*.public_ip
+}
